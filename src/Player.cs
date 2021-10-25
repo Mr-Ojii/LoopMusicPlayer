0142678125857aed1acd09445450969e27e0e84c
Mr-Ojii <okaschan@gmail.com>
2021-10-25 09:48:58 +0900

    Access Violationが出るので、とりあえずlockをする

--- src/Player.cs
+++ src/Player.cs
@@ -9,260 +9,267 @@ using LoopMusicPlayer.TagReaderExtensionMethods;
 
 namespace LoopMusicPlayer
 {
-	internal class Player : IDisposable
-	{
-		private IMusicFileReader reader = null;
-		private int StreamHandle = -1;
-		private StreamProcedure tSTREAMPROC = null;
-		private SyncProcedure tSYNCPROC = null;
-		public readonly bool IsLoop = false;
-		public readonly long LoopStart = 0;
-		public readonly long LoopEnd = 0;
-		public bool NextIsLoop = true;
-		public uint LoopCount
-		{
-			get;
-			private set;
-		} = 0;
+    internal class Player : IDisposable
+    {
+        private IMusicFileReader reader = null;
+        private int StreamHandle = -1;
+        private StreamProcedure tSTREAMPROC = null;
+        private SyncProcedure tSYNCPROC = null;
+        public readonly bool IsLoop = false;
+        public readonly long LoopStart = 0;
+        public readonly long LoopEnd = 0;
+        public bool NextIsLoop = true;
+        public uint LoopCount
+        {
+            get;
+            private set;
+        } = 0;
 
-		public TimeSpan LoopStartTime
-		{
-			get
-			{
-				double time = (LoopStart / (double)reader.SampleRate);
-				int millisecond = (int)((time % 1) * 1000);
-				int second = (int)(time % 60);
-				int minute = (int)(time / 60) % 60;
-				int hour = (int)(time / 3600) % 24;
-				int day = (int)(time / 86400);
-				return new TimeSpan(day, hour, minute, second, millisecond);
-			}
-		}
+        public TimeSpan LoopStartTime
+        {
+            get
+            {
+                double time = (LoopStart / (double)reader.SampleRate);
+                int millisecond = (int)((time % 1) * 1000);
+                int second = (int)(time % 60);
+                int minute = (int)(time / 60) % 60;
+                int hour = (int)(time / 3600) % 24;
+                int day = (int)(time / 86400);
+                return new TimeSpan(day, hour, minute, second, millisecond);
+            }
+        }
 
-		public TimeSpan LoopEndTime
-		{
-			get
-			{
-				double time = (LoopEnd / (double)reader.SampleRate);
-				int millisecond = (int)((time % 1) * 1000);
-				int second = (int)(time % 60);
-				int minute = (int)(time / 60) % 60;
-				int hour = (int)(time / 3600) % 24;
-				int day = (int)(time / 86400);
-				return new TimeSpan(day, hour, minute, second, millisecond);
-			}
-		}
+        public TimeSpan LoopEndTime
+        {
+            get
+            {
+                double time = (LoopEnd / (double)reader.SampleRate);
+                int millisecond = (int)((time % 1) * 1000);
+                int second = (int)(time % 60);
+                int minute = (int)(time / 60) % 60;
+                int hour = (int)(time / 3600) % 24;
+                int day = (int)(time / 86400);
+                return new TimeSpan(day, hour, minute, second, millisecond);
+            }
+        }
 
 
-		public long TotalSamples
-		{
-			get
-			{
-				return reader.TotalSamples;
-			}
-		}
+        public long TotalSamples
+        {
+            get
+            {
+                return reader.TotalSamples;
+            }
+        }
 
-		public long SamplePosition
-		{
-			get
-			{
-				return reader.SamplePosition;
-			}
-		}
+        public long SamplePosition
+        {
+            get
+            {
+                lock (LockObj)
+                {
+                    return reader.SamplePosition;
+                }
+            }
+            }
 
-		public TimeSpan TotalTime
-		{
-			get
-			{
-				return reader.TotalTime;
-			}
-		}
+        public TimeSpan TotalTime
+        {
+            get
+            {
+                return reader.TotalTime;
+            }
+        }
 
-		public TimeSpan TimePosition
-		{
-			get
-			{
-				return reader.TimePosition;
-			}
-		}
+        public TimeSpan TimePosition
+        {
+            get
+            {
+                lock (LockObj)
+                {
+                    return reader.TimePosition;
+                }
+            }
+        }
 
-		public string Title
-		{
-			get
-			{
-				return !string.IsNullOrEmpty(reader.Tags.Title) ? reader.Tags.Title : System.IO.Path.GetFileName(FilePath);
-			}
-		}
+        public string Title
+        {
+            get
+            {
+                return !string.IsNullOrEmpty(reader.Tags.Title) ? reader.Tags.Title : System.IO.Path.GetFileName(FilePath);
+            }
+        }
 
-		public string Artist
-		{
-			get
-			{
-				return !string.IsNullOrEmpty(reader.Tags.Artist) ? reader.Tags.Artist : "";
-			}
-		}
+        public string Artist
+        {
+            get
+            {
+                return !string.IsNullOrEmpty(reader.Tags.Artist) ? reader.Tags.Artist : "";
+            }
+        }
 
-		public event EventHandler LoopAction;
+        public event EventHandler LoopAction;
 
-		public Action EndAction;
+        public Action EndAction;
 
-		public readonly string FilePath;
+        public readonly string FilePath;
 
-		private bool Ended;
+        private bool Ended;
 
-		public Player(string filepath, double volume, bool streaming)
-		{
-			this.FilePath = filepath;
-			Ended = false;
-			if (streaming)
-				this.reader = new MusicFileReaderStreaming(filepath);
-			else
-				this.reader = new MusicFileReader(filepath);
+        private object LockObj = new object();
 
-			this.IsLoop = !string.IsNullOrEmpty(reader.Tags.GetTag("LOOPSTART")) && (!string.IsNullOrEmpty(reader.Tags.GetTag("LOOPLENGTH")) || !string.IsNullOrEmpty(reader.Tags.GetTag("LOOPEND")));
-			if (this.IsLoop)
-			{
-				LoopStart = long.Parse(reader.Tags.GetTag("LOOPSTART"));
-				if (!string.IsNullOrEmpty(reader.Tags.GetTag("LOOPLENGTH")))
-				{
-					LoopEnd = LoopStart + long.Parse(reader.Tags.GetTag("LOOPLENGTH"));
-				}
-				else
-				{
-					LoopEnd = long.Parse(reader.Tags.GetTag("LOOPEND"));
-				}
-			}
-			else
-			{
-				LoopStart = 0;
-				LoopEnd = reader.TotalSamples;
-			}
+        public Player(string filepath, double volume, bool streaming)
+        {
+            this.FilePath = filepath;
+            Ended = false;
+            if (streaming)
+                this.reader = new MusicFileReaderStreaming(filepath);
+            else
+                this.reader = new MusicFileReader(filepath);
 
-			this.tSTREAMPROC = new StreamProcedure(this.StreamProc);
-			this.tSYNCPROC = new SyncProcedure(this.EndProc);
-			this.StreamHandle = Bass.CreateStream(reader.SampleRate, reader.Channels, BassFlags.Float, this.tSTREAMPROC);
-			ChangeVolume(volume);
-			Bass.ChannelSetSync(this.StreamHandle, SyncFlags.Stalled, 0, this.tSYNCPROC);
+            this.IsLoop = !string.IsNullOrEmpty(reader.Tags.GetTag("LOOPSTART")) && (!string.IsNullOrEmpty(reader.Tags.GetTag("LOOPLENGTH")) || !string.IsNullOrEmpty(reader.Tags.GetTag("LOOPEND")));
+            if (this.IsLoop)
+            {
+                LoopStart = long.Parse(reader.Tags.GetTag("LOOPSTART"));
+                if (!string.IsNullOrEmpty(reader.Tags.GetTag("LOOPLENGTH")))
+                {
+                    LoopEnd = LoopStart + long.Parse(reader.Tags.GetTag("LOOPLENGTH"));
+                }
+                else
+                {
+                    LoopEnd = long.Parse(reader.Tags.GetTag("LOOPEND"));
+                }
+            }
+            else
+            {
+                LoopStart = 0;
+                LoopEnd = reader.TotalSamples;
+            }
 
-			Bass.ChannelPlay(this.StreamHandle);
-		}
+            this.tSTREAMPROC = new StreamProcedure(this.StreamProc);
+            this.tSYNCPROC = new SyncProcedure(this.EndProc);
+            this.StreamHandle = Bass.CreateStream(reader.SampleRate, reader.Channels, BassFlags.Float, this.tSTREAMPROC);
+            ChangeVolume(volume);
+            Bass.ChannelSetSync(this.StreamHandle, SyncFlags.Stalled, 0, this.tSYNCPROC);
 
-		public void Seek(long sample)
-		{
-			PlaybackState state = Bass.ChannelIsActive(this.StreamHandle);
-			Bass.ChannelPause(this.StreamHandle);
-			this.reader.SamplePosition = sample;
+            Bass.ChannelPlay(this.StreamHandle);
+        }
 
-			if (sample >= this.LoopEnd)
-				this.NextIsLoop = false;
+        public void Seek(long sample)
+        {
+            PlaybackState state = Bass.ChannelIsActive(this.StreamHandle);
+            Bass.ChannelPause(this.StreamHandle);
+            this.reader.SamplePosition = sample;
 
-			if (state == PlaybackState.Playing)
-				Bass.ChannelPlay(this.StreamHandle);
-		}
+            if (sample >= this.LoopEnd)
+                this.NextIsLoop = false;
 
-		public void ChangeVolume(double volume)//0~1
-		{
-			Bass.ChannelSetAttribute(this.StreamHandle, ChannelAttribute.Volume, volume);
-		}
+            if (state == PlaybackState.Playing)
+                Bass.ChannelPlay(this.StreamHandle);
+        }
 
-		public void Play()
-		{
-			this.reader.SamplePosition = 0;
-			Bass.ChannelPlay(this.StreamHandle);
-		}
+        public void ChangeVolume(double volume)//0~1
+        {
+            Bass.ChannelSetAttribute(this.StreamHandle, ChannelAttribute.Volume, volume);
+        }
 
-		public void Pause()
-		{
-			switch (Bass.ChannelIsActive(this.StreamHandle))
-			{
-				case PlaybackState.Paused:
-					Bass.ChannelPlay(this.StreamHandle);
-					break;
-				case PlaybackState.Playing:
-					Bass.ChannelPause(this.StreamHandle);
-					break;
-				default:
-					break;
-			}
-		}
+        public void Play()
+        {
+            this.reader.SamplePosition = 0;
+            Bass.ChannelPlay(this.StreamHandle);
+        }
 
-		public void Stop()
-		{
-			Bass.ChannelStop(this.StreamHandle);
-			this.reader.SamplePosition = 0;
-		}
+        public void Pause()
+        {
+            switch (Bass.ChannelIsActive(this.StreamHandle))
+            {
+                case PlaybackState.Paused:
+                    Bass.ChannelPlay(this.StreamHandle);
+                    break;
+                case PlaybackState.Playing:
+                    Bass.ChannelPause(this.StreamHandle);
+                    break;
+                default:
+                    break;
+            }
+        }
 
-		public PlaybackState Status()
-		{
-			return Bass.ChannelIsActive(this.StreamHandle);
-		}
+        public void Stop()
+        {
+            Bass.ChannelStop(this.StreamHandle);
+            this.reader.SamplePosition = 0;
+        }
 
-		public bool CheckDeviceEnable()
-		{
-			if (Bass.GetDeviceInfo(Bass.CurrentDevice, out DeviceInfo info))
-			{
-				return info.IsEnabled;
-			}
-			return false;
-		}
+        public PlaybackState Status()
+        {
+            return Bass.ChannelIsActive(this.StreamHandle);
+        }
 
-		public int StreamProc(int handle, IntPtr buffer, int length, IntPtr user)
-		{
-			int num;
-			int floatlength = (int)(Const.byte_per_float * length);
-			float[] tmp = new float[floatlength];
+        public bool CheckDeviceEnable()
+        {
+            if (Bass.GetDeviceInfo(Bass.CurrentDevice, out DeviceInfo info))
+            {
+                return info.IsEnabled;
+            }
+            return false;
+        }
 
-			if (NextIsLoop && reader.SamplePosition + floatlength > LoopEnd)
-			{
-				int tmplength = (int)(LoopEnd - reader.SamplePosition);
-				num = reader.ReadSamples(tmp, 0, tmplength);
-				reader.SamplePosition = LoopStart;
-				num = reader.ReadSamples(tmp, tmplength, floatlength - tmplength);
-				num = length;
-				this.LoopCount++;
-				if (LoopAction != null) LoopAction(this, EventArgs.Empty);
-			}
-			else
-			{
-				num = (int)(reader.ReadSamples(tmp, 0, tmp.Length) * Const.float_per_byte);
-			}
+        public int StreamProc(int handle, IntPtr buffer, int length, IntPtr user)
+        {
+            int num;
+            float[] tmp = new float[(int)(Const.byte_per_float * length)];
 
-			if (num < 0) num = 0;
-			if (num != 0) num = length;
+            if (NextIsLoop && reader.SamplePosition + tmp.Length >= LoopEnd)
+            {
+                int tmplength = (int)(LoopEnd - reader.SamplePosition);
+                num = reader.ReadSamples(tmp, 0, tmplength);
+                reader.SamplePosition = LoopStart;
+                num = reader.ReadSamples(tmp, tmplength, tmp.Length - tmplength);
+                num = length;
+                this.LoopCount++;
+                if (LoopAction != null) LoopAction(this, EventArgs.Empty);
+            }
+            else
+            {
+                num = (int)(reader.ReadSamples(tmp, 0, tmp.Length) * Const.float_per_byte);
+            }
 
-			unsafe
-			{
-				fixed (float* point = tmp)
-					Buffer.MemoryCopy(point, (float*)buffer, length, num);
-			}
+            if (num < 0) num = 0;
+            if (num != 0) num = length;
 
-			return num;
-		}
+            unsafe
+            {
+                fixed (float* point = tmp)
+                    Buffer.MemoryCopy(point, (float*)buffer, length, num);
+            }
 
-		public void EndProc(int handle, int channel, int data, IntPtr user)
-		{
-			if (!Ended)
-			{
-				Ended = true;
-				this.EndAction();
-			}
-		}
+            return num;
+        }
 
-		public void Dispose() 
-		{
-			try
-			{
-				if (this.StreamHandle != -1)
-				{
-					Bass.ChannelStop(this.StreamHandle);
-					Bass.StreamFree(this.StreamHandle);
-				}
-				reader?.Dispose();
-			}
-			catch (Exception e) 
-			{
-				Trace.WriteLine(e);
-			}
-		}
-	}
+        public void EndProc(int handle, int channel, int data, IntPtr user)
+        {
+            if (!Ended)
+            {
+                Ended = true;
+                this.EndAction();
+            }
+        }
+
+        public void Dispose() 
+        {
+            try
+            {
+                if (this.StreamHandle != -1)
+                {
+                    Bass.ChannelStop(this.StreamHandle);
+                    Bass.StreamFree(this.StreamHandle);
+                }
+                reader?.Dispose();
+            }
+            catch (Exception e) 
+            {
+                Trace.WriteLine(e);
+            }
+        }
+    }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -7,38 +7,38 @@ using Gtk;
 
 namespace LoopMusicPlayer
 {
-	internal class Program
-	{
-		[STAThread]
-		public static void Main(string[] args)
-		{
-			try
-			{
-				string osplatform = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "win" : (RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ? "osx" : "linux");
-				string platform = Environment.Is64BitProcess ? "x64" : "x86";
-				DirectoryInfo info = new DirectoryInfo(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + @"/dll/" + osplatform + "-" + platform + "/");
+    internal class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            try
+            {
+                string osplatform = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "win" : (RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ? "osx" : "linux");
+                string platform = Environment.Is64BitProcess ? "x64" : "x86";
+                DirectoryInfo info = new DirectoryInfo(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + @"/dll/" + osplatform + "-" + platform + "/");
 
-				//exeÇÃäKëwÇ…dllÇÉRÉsÅ[
-				foreach (FileInfo fileinfo in info.GetFiles())
-				{
-					fileinfo.CopyTo(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "/" + fileinfo.Name, true);
-				}
-			}
-			catch (Exception e)
-			{
-				Trace.TraceError(e.ToString());
-			}
+                //exeÇÃäKëwÇ…dllÇÉRÉsÅ[
+                foreach (FileInfo fileinfo in info.GetFiles())
+                {
+                    fileinfo.CopyTo(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "/" + fileinfo.Name, true);
+                }
+            }
+            catch (Exception e)
+            {
+                Trace.TraceError(e.ToString());
+            }
 
-			Application.Init();
+            Application.Init();
 
-			var app = new Application("Mr-Ojii.LoopMusicPlayer.LoopMusicPlayer", GLib.ApplicationFlags.None);
-			app.Register(GLib.Cancellable.Current);
+            var app = new Application("Mr-Ojii.LoopMusicPlayer.LoopMusicPlayer", GLib.ApplicationFlags.None);
+            app.Register(GLib.Cancellable.Current);
 
-			var win = new MainWindow();
-			app.AddWindow(win);
+            var win = new MainWindow();
+            app.AddWindow(win);
 
-			win.Show();
-			Application.Run();
-		}
-	}
+            win.Show();
+            Application.Run();
+        }
+    }
 }
