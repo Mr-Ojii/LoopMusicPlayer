a30c0d92153c1616465df8123fcdc6ed429e49ab
Mr-Ojii <okaschan@gmail.com>
2021-11-11 20:18:48 +0900

    実行ファイルパスの取得方法をAppContext.BaseDirectoryに変更

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,5 @@
 using System;
 using System.IO;
-using System.Reflection;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using Gtk;
@@ -16,12 +15,12 @@ namespace LoopMusicPlayer
             {
                 string osplatform = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "win" : (RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ? "osx" : "linux");
                 string platform = Environment.Is64BitProcess ? "x64" : "x86";
-                DirectoryInfo info = new DirectoryInfo(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + @"/dll/" + osplatform + "-" + platform + "/");
+                DirectoryInfo info = new DirectoryInfo(AppContext.BaseDirectory + @"dll/" + osplatform + "-" + platform + "/");
 
                 //exeÇÃäKëwÇ…dllÇÉRÉsÅ[
                 foreach (FileInfo fileinfo in info.GetFiles())
                 {
-                    fileinfo.CopyTo(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "/" + fileinfo.Name, true);
+                    fileinfo.CopyTo(AppContext.BaseDirectory + fileinfo.Name, true);
                 }
             }
             catch (Exception e)
