09830d8c52b90961a9fd888b991e7a1ef3436557
Mr-Ojii <okaschan@gmail.com>
2023-01-25 10:04:49 +0900

    submoduleへの依存を削除

---
+++ LoopMusicPlayer/Core/Const.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopMusicPlayer.Core
+{
+    internal class Const
+    {
+        public const double byte_per_float = sizeof(byte) / (double)sizeof(float);
+        public const double float_per_byte = sizeof(float) / (double)sizeof(byte);
+    }
+}
---
+++ LoopMusicPlayer/Core/MusicFileReader/IMusicFileReader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ManagedBass;
+
+namespace LoopMusicPlayer.Core
+{
+    public interface IMusicFileReader : IDisposable 
+    {
+        long TotalSamples { get; }
+        TimeSpan TotalTime { get; }
+        TagReader Tags { get; }
+        int SampleRate { get; }
+        int Channels { get; }
+        long SamplePosition { get; set; }
+        TimeSpan TimePosition { get; }
+
+        int ReadSamples(IntPtr buffer, int byte_offset, int byte_count);
+    }
+}
