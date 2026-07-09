e145bb98e314a9e1571e463f7de8bcee6bec10bc
Mr-Ojii <okaschan@gmail.com>
2026-07-10 00:56:59 +0900

    Update Avalonia UI and FluentIcons.

--- LoopMusicPlayer/App.axaml.cs
+++ LoopMusicPlayer/App.axaml.cs
@@ -28,9 +28,6 @@ public partial class App : Application
         Assets.Resources.Culture = CultureInfo.CurrentCulture;
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            // Line below is needed to remove Avalonia data validation.
-            // Without this line you will get duplicate validations from both Avalonia and CT
-            BindingPlugins.DataValidators.RemoveAt(0);
             desktop.MainWindow = new MainWindow
             {
                 DataContext = new MainViewModel()
